f2b74169a059686e15a034e1e8700ea6f840ffab
AndreyMustafin2001 <andrey.must2001@gmail.com>
2023-11-15 00:28:07 +0400

    init

---
+++ unordered_map.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+int main()
+{
+    std :: string ransomLetter, paper;
+
+    std :: cout << "ransomLetter: ";
+    std :: getline(std::cin, ransomLetter);
+
+    std :: cout << "paper: ";
+    std :: getline(std::cin, paper);
+
+    std :: unordered_map<char, int> charCount1;
+
+    for (char S : paper)
+    {
+        charCount1[S]++;
+    }
+    std :: unordered_map<char, int> charCount2;
+    for (char S : ransomLetter)
+    {
+        charCount2[S]++;
+    }
+    bool all = true;
+    for (const auto& A : charCount2)
+    {
+        char S = A.first;
+        int Count2 = A.second;
+
+        if (charCount1[S] < Count2)
+        {
+            all = false;
+            break;
+        }
+    }
+    std :: cout << "\nResult: " << std :: boolalpha << all << std::endl;
+    return 0;
+}
