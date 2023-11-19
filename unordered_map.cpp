9777b8960bc2a84ff9c56aa9c7824729fc10e805
AndreyMustafin2001 <150649500+AndreyMustafin2001@users.noreply.github.com>
2023-11-19 14:00:12 +0400

    N2

--- unordered_map.cpp
+++ unordered_map.cpp
@@ -1,46 +1,51 @@
 #include <iostream>
 #include <unordered_map>
 #include <string>
-bool Function(std :: string ransomLetter,std :: string paper)
-{
+
+bool isitLetter(std :: string ransomLetter,std :: string paper) {
+
     std :: unordered_map<char, int> charCount1;
-    for (char S : paper)
-    {
+
+    for (char S : paper) {
         charCount1[S]++;
     }
+
     std :: unordered_map<char, int> charCount2;
-    for (char S : ransomLetter)
-    {
+
+    for (char S : ransomLetter) {
         if(S == ' ')continue;
         charCount2[S]++;
     }
+
     bool all = true;
-    for (const auto& A : charCount2)
-    {
+    for (const auto& A : charCount2) {
         char S = A.first;
         int Count2 = A.second;
 
-        if (charCount1[S] < Count2)
-        {
+        if (charCount1[S] < Count2) {
             all = false;
             break;
         }
     }
     return all;
 }
+
 int main()
 {
     std :: string ransomLetter, paper;
+
     std :: cout << "ransomLetter: ";
     std :: getline(std::cin, ransomLetter);
+
     std :: cout << "paper: ";
     std :: getline(std::cin, paper);
+
     std :: unordered_map<char, int> charCount1;
 
-    if(Function(ransomLetter, paper)==true)
-        std :: cout << "true"<< std :: endl;
+    if(isitLetter(ransomLetter, paper) == true)
+        std :: cout << "true" << std :: endl;
     else
-        std :: cout << "false"<< std :: endl;
+        std :: cout << "false" << std :: endl;
 
     return 0;
 }
